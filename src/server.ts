5e233ec5c01139544f474c2a7ffae0c2115250c6
SilasEduardo <silasandrade94@gmail.com>
2023-03-02 18:10:10 -0300

    structure

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+app.listen(3333, ()=> {
+  console.log(`server running on CTRL + CLICK in ${'http://localhost:3333/'}`)
+})
