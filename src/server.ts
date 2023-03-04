c4dea7b07daec4f56e52c4287d7592947a6404b1
SilasEduardo <silasandrade94@gmail.com>
2023-03-03 23:58:08 -0300

    updade

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,11 @@
 import express from 'express';
 import cors from 'cors';
-import { studantRouter } from './routers/studants/studantesRouter';
+
 
 const app = express()
 app.use(express.json())
 app.use(cors())
 
-app.use("/studant", studantRouter);
 
 app.listen(3333, ()=> {
   console.log(`server running on CTRL + CLICK in ${'http://localhost:3333/'}`)
