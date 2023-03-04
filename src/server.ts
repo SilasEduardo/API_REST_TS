d3d8618d7cf8b374488fe0b48576c2d66141edf3
SilasEduardo <silasandrade94@gmail.com>
2023-03-04 00:02:58 -0300

    create router studant

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 import cors from 'cors';
+import { studantRouters } from './routers/studant/studantRouter';
 
 
 const app = express()
 app.use(express.json())
 app.use(cors())
-
+app.use("/studant", studantRouters);
 
 app.listen(3333, ()=> {
   console.log(`server running on CTRL + CLICK in ${'http://localhost:3333/'}`)
