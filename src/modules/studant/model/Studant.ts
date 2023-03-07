be1e913d6bb02bdc18cfcd6f45cfc37a9c828232
SilasEduardo <silasandrade94@gmail.com>
2023-03-07 10:44:04 -0300

    refatoration model

--- src/modules/studant/model/Studant.ts
+++ src/modules/studant/model/Studant.ts
@@ -1,16 +1,16 @@
 import {v4 as uuidv4} from  'uuid'
 
 
+
+
 class Studant {
-
-  id?: string;
-  firstName!: string;
-  lestName!: string;
-  email!: string;
-  age!: number;
-  status!: boolean;
-  create_ad!: Date;
-
+  firstName: string;
+  lestName: string;
+  email: string;
+  age: number;
+  status: boolean;
+  create_ad: Date;
+  id: string;
 
 constructor(){
   if(!this.id){
